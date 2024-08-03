f8a3f6e3683ef316c11af43e211ba6a302751716
teramako <teramako@gmail.com>
2024-08-04 01:33:10 +0900

    :fish: fix: remove unecessary `--preset`

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 
 if status is-interactive
     abbr -a vi vim
-    bind --preset \cg\cf forward-bigword
-    bind --preset \cg\cb backward-bigword
+    bind \cg\cf forward-bigword
+    bind \cg\cb backward-bigword
 
     complete -f	-c dotnet -a "(dotnet complete (commandline -cp))"
 end
