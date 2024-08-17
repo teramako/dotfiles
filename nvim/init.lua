bd517fe187919f3c4cdbb20974c2782c4c8c5698
teramako <teramako@gmail.com>
2024-08-18 01:44:55 +0900

    :fire: コメントアウトしていた`mini.completion`設定を削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -328,37 +328,6 @@ end)
 -- }}}
 
 -- Auto Completion {{{
----- min.completion {{{
--- later(function()
---     require('mini.completion').setup({
---         -- delay = { completion = 100, info = 100, signature = 50 },
---         -- window = {
---         --     info = { height = 25, width = 80, border = 'double' },
---         --     signature = { height = 25, width = 80, border = 'single' },
---         -- },
---         -- lsp_completion = {
---         --     -- source_func = 'completefunc',
---         --     source_func = 'omnifunc',
---         -- },
---         -- auto_setup = true,
---         -- set_vim_settings = true
---     })
---     vim.keymap.set('i', '<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]],   { expr = true })
---     vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
---     local keys = {
---         ['cr'] = vim.api.nvim_replace_termcodes('<CR>', true, true, true),
---         ['ctrl-y'] = vim.api.nvim_replace_termcodes('<C-y>', true, true, true),
---         ['ctrl-y_cr'] = vim.api.nvim_replace_termcodes('<C-y><CR>', true, true, true),
---     }
---     _G.cr_action = function()
---         if vim.fn.pumvisible() ~= 0 then
---             local item_selected = vim.fn.complete_info()['selected'] ~= -1
---             return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
---         else
---             return keys['cr']
---         end
---     end
--- end) -- }}}
 -- nvim-cmp {{{
 later(function()
     add({
