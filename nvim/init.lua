7206623c3388b9ba6af6d0014ef5160c97f240c2
teramako <teramako@gmail.com>
2024-08-23 23:22:46 +0900

    :art: :neovim: LSP hover/signature_help のポップアップに枠線を付ける

--- nvim/init.lua
+++ nvim/init.lua
@@ -212,6 +212,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
             vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
         )
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+            vim.lsp.handlers.hover, { border = 'rounded' }
+        )
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+            vim.lsp.handlers.signature_help, { border = 'single' }
+        )
     end
 })
 
