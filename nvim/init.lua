5ea9904c4ee0962653803a5d1f47c44851b83120
teramako <teramako@gmail.com>
2024-10-26 19:00:41 +0900

    :neovim: omnisharp 設定

--- nvim/init.lua
+++ nvim/init.lua
@@ -312,6 +312,52 @@ now(function()
                 }
             })
         end,
+        ['omnisharp'] = function()
+            local util = require('lspconfig.util');
+            require('lspconfig').omnisharp.setup({
+                cmd = { "/usr/bin/dotnet", vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+                settings = {
+                    FormattingOptions = {
+                        -- Enables support for reading code style, naming convention and analyzer
+                        -- settings from .editorconfig.
+                        EnableEditorConfigSupport = true,
+                        -- Specifies whether 'using' directives should be grouped and sorted during
+                        -- document formatting.
+                        OrganizeImports = nil,
+                    },
+                    MsBuild = {
+                        -- If true, MSBuild project system will only load projects for files that
+                        -- were opened in the editor. This setting is useful for big C# codebases
+                        -- and allows for faster initialization of code navigation features only
+                        -- for projects that are relevant to code that is being edited. With this
+                        -- setting enabled OmniSharp may load fewer projects and may thus display
+                        -- incomplete reference lists for symbols.
+                        LoadProjectsOnDemand = nil,
+                    },
+                    RoslynExtensionsOptions = {
+                        -- Enables support for roslyn analyzers, code fixes and rulesets.
+                        EnableAnalyzersSupport = true,
+                        -- Enables support for showing unimported types and unimported extension
+                        -- methods in completion lists. When committed, the appropriate using
+                        -- directive will be added at the top of the current file. This option can
+                        -- have a negative impact on initial completion responsiveness,
+                        -- particularly for the first few completion sessions after opening a
+                        -- solution.
+                        EnableImportCompletion = nil,
+                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                        -- true
+                        AnalyzeOpenDocumentsOnly = nil,
+                    },
+                    Sdk = {
+                        -- Specifies whether to include preview versions of the .NET SDK when
+                        -- determining which version to use for project loading.
+                        IncludePrereleases = false,
+                    },
+                },
+                filetypes = { 'cs' },
+                root_dir = util.root_pattern('*.sln', '*.csproj', 'omnisharp.json'),
+            })
+        end,
         -- XML 系のLSP
         --  * https://github.com/eclipse/lemminx
         --  * https://github.com/redhat-developer/vscode-xml/blob/main/docs/Preferences.md
