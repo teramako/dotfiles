f1b04572296bc0bf4afcd515bb5930c5f7b785c7
teramako <teramako@gmail.com>
2026-04-05 20:11:28 +0900

    [:neovim:] lemminx LS設定修正

--- nvim/after/lsp/lemminx.lua
+++ nvim/after/lsp/lemminx.lua
@@ -2,7 +2,7 @@
 --  * https://github.com/eclipse/lemminx
 --  * https://github.com/redhat-developer/vscode-xml/blob/main/docs/Preferences.md
 return {
-    filetypes = { 'xml', 'xsd', 'xsl', 'xslt', 'svg', 'ps1xml' },
+    filetypes = { 'xml', 'xsd', 'xslt', 'svg', 'ps1xml' },
     settings = {
         xml = {
             server = {
@@ -21,6 +21,10 @@ return {
                     pattern = '**/*-Help.xml',
                     -- systemId = 'https://raw.githubusercontent.com/PowerShell/PowerShell/refs/heads/master/src/Schemas/PSMaml/Maml.xsd'
                     systemId = vim.fn.stdpath("config") .. '/../xsd/PSMaml.xsd'
+                },
+                {
+                    pattern = '**/*.resx',
+                    systemId = vim.fn.stdpath("config") .. '/../xsd/resx.xsd'
                 }
             },
             format = {
